70a18d28afa9777882be609e98759ad375dd3b00
sarahannata <sarahannata@gmail.com>
2024-01-10 00:29:38 +0700

    add RepositoriBarang.kt

---
+++ app/src/main/java/com/example/madoora/repositori/RepositoriBarang.kt
@@ -0,0 +1,17 @@
+package com.example.madoora.repositori
+
+import com.example.madoora.data.Admin
+import com.example.madoora.data.Barang
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriBarang {
+    fun getAllBarangStream(): Flow<List<Barang>>
+
+    fun getBarangStream(id : Int): Flow<Barang?>
+
+    suspend fun insertBarang(barang: Barang)
+
+    suspend fun deleteBarang(barang: Barang)
+
+    suspend fun updateBarang(barang: Barang)
+}
