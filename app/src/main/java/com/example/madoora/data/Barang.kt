f7fa238b75b4809c9853348cf1198d6974e4e15d
sarahannata <sarahannata@gmail.com>
2024-01-10 00:04:44 +0700

    add Barang.kt

---
+++ app/src/main/java/com/example/madoora/data/Barang.kt
@@ -0,0 +1,13 @@
+package com.example.madoora.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblBarang")
+data class Barang (
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val namaBarang : String,
+    val jumlah : Int,
+    val harga : Int
+)
