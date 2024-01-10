039e3ff302296a42ac7df7a2217066a3ed34bd19
sarahannata <sarahannata@gmail.com>
2024-01-11 01:02:49 +0700

    edit DatabaseMadoora

--- app/src/main/java/com/example/madoora/data/DatabaseMadoora.kt
+++ app/src/main/java/com/example/madoora/data/DatabaseMadoora.kt
@@ -5,9 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Admin::class, Barang::class], version = 1, exportSchema = false)
+@Database(entities = [ Barang::class], version = 1, exportSchema = false)
 abstract class DatabaseMadoora : RoomDatabase() {
-    abstract fun AdminDao() : AdminDao
+
     abstract fun BarangDao() : BarangDao
 
     companion object{
