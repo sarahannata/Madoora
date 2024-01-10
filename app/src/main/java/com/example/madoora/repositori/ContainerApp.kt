36e1347b6d2457522b15aa3db01e5b8bbc1c3d1e
sarahannata <sarahannata@gmail.com>
2024-01-11 01:02:32 +0700

    edit ContainerApp.kt

--- app/src/main/java/com/example/madoora/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/madoora/repositori/ContainerApp.kt
@@ -4,19 +4,12 @@ import android.content.Context
 import com.example.madoora.data.DatabaseMadoora
 
 interface ContainerApp {
-    val repositoriAdmin : RepositoriAdmin
     val repositoriBarang : RepositoriBarang
 
 }
-
 class ContainerDataApp(private val context: Context) : ContainerApp {
-    private val database = DatabaseMadoora.getDatabase(context)
-
-    override val repositoriAdmin : RepositoriAdmin by lazy {
-        OfflineRepositoriMadoora(database.AdminDao(), database.BarangDao())
-    }
 
     override val repositoriBarang: RepositoriBarang by lazy {
-        OfflineRepositoriMadoora(database.AdminDao(), database.BarangDao())
+        OfflineRepositoriMadoora(DatabaseMadoora.getDatabase(context).BarangDao())
     }
 }
