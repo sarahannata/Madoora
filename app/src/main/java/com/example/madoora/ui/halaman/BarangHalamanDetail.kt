dc178622b2033436f3dd8bcd849733abf2601d96
sarahannata <sarahannata@gmail.com>
2024-01-11 01:02:09 +0700

    menambahkan BarangHalamanDetail.kt

--- app/src/main/java/com/example/madoora/ui/halaman/BarangHalamanDetail.kt
+++ app/src/main/java/com/example/madoora/ui/halaman/BarangHalamanDetail.kt
@@ -1,6 +1,11 @@
 package com.example.madoora.ui.halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.madoora.R
+import com.example.madoora.model.BarangDetailsViewModel
+import com.example.madoora.model.PenyediaViewModel
 import com.example.madoora.navigasi.DestinasiNavigasi
 
 object BarangDetailsDestination : DestinasiNavigasi {
@@ -8,4 +13,13 @@ object BarangDetailsDestination : DestinasiNavigasi {
     override val titleRes = R.string.detail_barang
     const val barangIdArg = "itemId"
     val routeWithArgs = "$route/{$barangIdArg}"
+}
+@Composable
+fun BarangDetailsScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: BarangDetailsViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
