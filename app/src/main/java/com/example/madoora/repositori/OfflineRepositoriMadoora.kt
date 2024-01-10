409a29c02d139eff75dfc65aac880a20afa74073
sarahannata <sarahannata@gmail.com>
2024-01-11 01:03:20 +0700

    edit OfflineRepositoriMadoora.kt

--- app/src/main/java/com/example/madoora/repositori/OfflineRepositoriMadoora.kt
+++ app/src/main/java/com/example/madoora/repositori/OfflineRepositoriMadoora.kt
@@ -1,29 +1,12 @@
 package com.example.madoora.repositori
 
-import com.example.madoora.data.Admin
-import com.example.madoora.data.AdminDao
 import com.example.madoora.data.Barang
 import com.example.madoora.data.BarangDao
 import kotlinx.coroutines.flow.Flow
 
 class OfflineRepositoriMadoora(
-    private val adminDao: AdminDao,
-    private val barangDao: BarangDao
-):RepositoriAdmin, RepositoriBarang {
-    override fun getAllAdminStream(): Flow<List<Admin>> {
-        return adminDao.getAllAdmin()
-    }
 
-    override fun getAdminStream(id: Int): Flow<Admin?> = adminDao.getAdmin(id)
-
-    override suspend fun insertAdmin(admin: Admin) {
-        adminDao.insertAdmin(admin)
-    }
-
-    override suspend fun deleteAdmin(admin: Admin) = adminDao.deleteAdmin(admin)
-
-    override suspend fun updateAdmin(admin: Admin) = adminDao.updateAdmin(admin)
-
+    private val barangDao: BarangDao): RepositoriBarang {
     override fun getAllBarangStream(): Flow<List<Barang>> {
         return barangDao.getAllBarang()
     }
